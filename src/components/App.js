73d08d772a7ad7e8754b07e333fb173d168154d2
Xu Ren <musicalxu@hotmail.com>
2019-08-01 14:59:22 -0400

    add Dashboard component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
+import Dashboard from './Dashboard'
+
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Starter Code
-      </div>
-    )
-  }
+    
+    componentDidMount() {
+       this.props.dispatch(handleInitialData()) 
+    }
+
+    render() {
+        return (
+            <div>
+            {
+                this.props.loading === true ? 
+                <h1>LOADING</h1> :
+                <Dashboard />
+            }
+            </div>
+        )
+    }
 }
 
-export default App
+function mapStateToProps({ authedUser }) {
+    return {
+        loading: authedUser === null
+    }
+}
+
+export default connect()(App)
---
+++ src/components/Dashboard.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Dashboard extends Component {
+    render() {
+        console.log(this.props.tweetIds)
+        return (
+            <div>
+                <h3 className='center'>Your Timeline</h3>
+                <ul className='dashboard-list'>
+                    {
+                        this.props.tweetIds.map((id) => (
+                            <li key={id}>
+                                {id}
+                            </li>
+                        ))
+                    }   
+                </ul>
+            </div>
+        )
+    }
+}
+
+
+function mapStateToProps({ tweets }) {
+    return {
+        tweetIds: Object.keys(tweets).sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
+    }
+}
+
+export default connect(mapStateToProps)(Dashboard)
+
